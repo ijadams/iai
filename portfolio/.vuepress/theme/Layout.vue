f075b487c8b20903b518a0738148dbd2b35c4a32
Ian Adams <ijadams503@gmail.com>
2021-08-16 13:47:36 -0500

    new font

--- portfolio/.vuepress/theme/Layout.vue
+++ portfolio/.vuepress/theme/Layout.vue
@@ -80,7 +80,9 @@
   }
 </script>
 
+
 <style>
+  @import url('https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap');
 
   :root {
     --color-black: #1c1c1c;
@@ -109,7 +111,7 @@
   }
 
   body {
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Noto Sans", "Ubuntu", "Droid Sans", "Helvetica Neue", sans-serif;
+    font-family: 'Nunito',  -apple-system, BlinkMacSystemFont, sans-serif;
     font-size: 16px;
     background: #fff;
     color: var(--color-black);
